55a5e6156fca912c396f63fa4be41bda762506c2
Dylan3009 <dylan.melendezl@hotmail.com>
2021-11-20 22:36:25 -0600

    ADD: Create, Read Functions

--- CRUD/client/src/App.js
+++ CRUD/client/src/App.js
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 import './App.css';
 
 function App() {
 
   const [countryName, setCountryName] = useState('');
+
+  const [countryList, setCountryList] = useState([]);
+
+  const [newCountryName, setNewCountryName] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setCountryList(response.data);
+    });
+  }, []);
 
   const addToList = () => {
     Axios.post("http://localhost:3001/insert", {
@@ -22,12 +32,36 @@ function App() {
           }}
         ></input>
         <div className="div-button">
-          <button className="send-button" 
-            onClick ={addToList}
+          <button className="send-button"
+            onClick={addToList}
           >
-            Enviar
+            Create
           </button>
         </div>
+
+        <h1>Country Names</h1>
+
+        {countryList.map((val, key) => {
+          return (
+            <div key={key}>
+              <h1>{val.countryName}</h1>
+              <input className="input_text"
+                type="text" placeholder="New Country"
+                onChange={(event) => {
+                  setCountryName(event.target.value);
+                }}
+              />
+              <button className="send-button"
+                onClick={updateCountry}
+              >
+                Update
+              </button>
+              <button className="send-button">
+                Delete
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- CRUD/server/index.js
+++ CRUD/server/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const CountryModel = require("./models/Country");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://newUser:1234@sed.3u2ye.mongodb.net/country?retryWrites=true&w=majority", {
     useNewUrlParser: true,
 });
 
-app.post('/insert', async(res, req) => {
-    const country = new CountryModel({countryName: "Finland"})
+app.post("/insert", async (req, res) => {
 
-    try{
+    const countryName = req.body.countryName;
+
+    const country = new CountryModel({ countryName: countryName });
+
+    try {
         await country.save();
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
-})
+});
+
+app.get("/read", async (req, res) => {
+    CountryModel.find({}, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+
+        res.send(result);
+    });
+});
 
 app.listen(3001, () => {
     console.log("Server running 3001");
