3a1912468d4b3c05181dbff4e8640644a8f89a55
Dylan3009 <dylan.melendezl@hotmail.com>
2021-11-20 19:33:50 -0600

    Add: CRUD Application

---
+++ CRUD/client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="source-game">
+        <label className="label_text">Agregar Usuario:</label>
+        <input
+          className="input_text"
+        ></input>
+        <div className="div-button">
+          <button className="send-button" >
+            Enviar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
